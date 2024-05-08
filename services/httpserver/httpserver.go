300b14bac406d44ac1831f5582f869a8d2e974d2
kimi <kimi0230@gmail.com>
2024-05-08 18:09:38 +0800

    feat: graceful shutdown http server

--- services/httpserver/httpserver.go
+++ services/httpserver/httpserver.go
@@ -2,9 +2,14 @@ package httpserver
 
 import (
 	"TaskRESTfulExercise/app/routes"
+	"context"
 	"log"
 	"net/http"
+	"os"
+	"os/signal"
 	"strconv"
+	"syscall"
+	"time"
 
 	"github.com/gin-gonic/gin"
 	"github.com/spf13/viper"
@@ -24,5 +29,33 @@ func StartGinServer(port int) {
 		WriteTimeout: viper.GetDuration("http.write_timeout"),
 	}
 
-	log.Fatal(httpServer.ListenAndServe())
+	// Wait for interrupt signal to gracefully shutdown the server with
+	// a timeout of 5 seconds.
+	quit := make(chan os.Signal, 1)
+	// listen SIGINT & SIGTERM signal
+	signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
+
+	// kill (no param) default send syscanll.SIGTERM
+	// kill -2 is syscall.SIGINT
+	// kill -9 is syscall. SIGKILL but can"t be catch, so don't need add it
+	signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
+
+	// start http server
+	go func() {
+		if err := httpServer.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+			log.Fatalf("listen: %s\n", err)
+		}
+	}()
+
+	<-quit
+	log.Println("Shutdown Server ...")
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+	if err := httpServer.Shutdown(ctx); err != nil {
+		log.Fatal("Server forced to shutdown:", err)
+	}
+
+	log.Println("Server exiting")
+
 }
